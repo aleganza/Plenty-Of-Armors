2f9fb016c4ccbe6859bb98009bad58056579ebdb
aleganza <ganzarolli.alessio@gmail.com>
2022-08-18 00:27:34 +0200

    icon

--- src/main/java/net/aleganza/plentyofarmors/item/ModItemGroup.java
+++ src/main/java/net/aleganza/plentyofarmors/item/ModItemGroup.java
@@ -9,5 +9,5 @@ import net.minecraft.util.Identifier;
 public class ModItemGroup {
 
     public static final ItemGroup PLENTYOFARMOR = FabricItemGroupBuilder.build(new Identifier(PlentyOfArmors.MOD_ID, "plentyofarmors"),
-            () -> new ItemStack(ModItems.MARINE_DIAMOND_CHESTPLATE));
+            () -> new ItemStack(ModItems.ADV_ICON));
 }
